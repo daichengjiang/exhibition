eabb7f93a75675c1f4ae900206788948c0609454
daichengjiang <13086623773@163.com>
2017-04-13 11:11:24 +0800

    增加操作日志表

--- exhibition/db/test.sql
+++ exhibition/db/test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2017-04-01 17:09:44
+Date: 2017-04-13 11:09:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -55,6 +55,25 @@ CREATE TABLE `sys_menu` (
 INSERT INTO `sys_menu` VALUES ('1', '1', 'qwe', 'sjflwlelke', 'skjfklewlk', 'slflwekj', '1', '1');
 
 -- ----------------------------
+-- Table structure for sys_operatelog
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_operatelog`;
+CREATE TABLE `sys_operatelog` (
+  `id` int(24) NOT NULL AUTO_INCREMENT COMMENT '操作日志ID',
+  `operateType` varchar(64) CHARACTER SET utf8 NOT NULL COMMENT '操作日志类型',
+  `operateContent` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '操作日志内容',
+  `operateTime` datetime NOT NULL COMMENT '操作时间',
+  `operateIP` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '操作IP',
+  `operator` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '操作者',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of sys_operatelog
+-- ----------------------------
+INSERT INTO `sys_operatelog` VALUES ('26', '重置密码', '重置管理后台登录密码为【加密前：123456】-【加密后：e10adc3949ba59abbe56e057f20f883e】', '2017-04-13 09:39:19', '0:0:0:0:0:0:0:1', 'admin');
+
+-- ----------------------------
 -- Table structure for sys_picture
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_picture`;
@@ -96,10 +115,10 @@ CREATE TABLE `sys_user` (
   `icon` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '头像',
   `lastIp` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '最后一次登录IP',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=112343 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES ('1', 'daicj', '111111', '13501234567', '13501234567@163.com', '2016-02-22 17:09:52', '1', '一直很安静', '', '');
-INSERT INTO `sys_user` VALUES ('2', 'admin', '111111', '13501234560', '13501234560@163.com', '2016-02-22 17:11:05', '1', ' 十口心思，思君思国思社稷', '', '');
+INSERT INTO `sys_user` VALUES ('2', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '13501234560', '13501234560@163.com', '2016-02-22 17:11:05', '1', ' 十口心思，思君思国思社稷', '', '0:0:0:0:0:0:0:1');
